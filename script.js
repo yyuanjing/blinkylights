921714a4b48049b173dceea6a973718bc0794eba
Glitch (magical-amplified-azimuth) <none>
2022-04-23 00:10:55 +0000

    final!

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
-const cluePauseTime = 333; //how long to pause in between clues
+var clueHoldTime = 1000; //how long to hold each clue's light/sound
+var cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
 //Global Variables
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var pattern = [2, 2, 4, 5, 3, 2, 1, 2, 4, 5];
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5;  //must be between 0.0 and 1.0
 var guessCounter = 0;
-var winPattern = [1, 2, 3, 4];
+var winPattern = [1, 2, 3, 4, 5];
+var strike = 0;
+//var timer = 10;
+
+// function decreaseTimer(){
+//   timer-=1;
+//   if(timer == 0){
+//     clearInterval();
+//     stopGame();
+//     alert("Out of time! Game over.")
+//   }
+// }
 
 function startGame(){
   //initialize game variables
   progress = 0;
   gamePlaying = true;
+  strike = 0;
+  //timer = 10;
+  //clearInterval();
+  for(let i=0; i<10; i++){
+    pattern[i] = Math.floor(Math.random()*5 +1);
+    console.log(pattern[i])
+  }
     
   // swap the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
@@ -24,9 +42,13 @@ function startGame(){
 }
 
 function stopGame(){
+  //clearInterval();
   gamePlaying = false;
+  clueHoldTime = 1000;
+  cluePauseTime = 333;
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
+  
   
 }
 
@@ -47,6 +69,8 @@ function playSingleClue(btn){
 }
 
 function playClueSequence(){
+  // clearInterval();
+  // timer = 10;
   guessCounter = 0;
   context.resume()
   let delay = nextClueWaitTime; //set delay to initial wait time
@@ -56,14 +80,17 @@ function playClueSequence(){
     delay += clueHoldTime 
     delay += cluePauseTime;
   }
+  clueHoldTime-=80;
+  cluePauseTime-=30;
+  //setInterval(decreaseTimer, 1000);
 }
 
 function playWinSequence(){
   let delay = nextClueWaitTime;
-  for(let i=0; i<=3; i++){
+  for(let i=0; i<=4; i++){
     setTimeout(playSingleClue,delay,winPattern[i])
-    delay += clueHoldTime
-    delay += cluePauseTime;
+    delay += 200
+    delay += 35;
   }
   setTimeout(stopGame, delay);
   setTimeout(alert, delay,"Game Over. You won!");
@@ -91,7 +118,15 @@ function guess(btn){
     }
   }
   else{
-    loseGame();
+    strike ++;
+    if(strike==3){
+      loseGame(); 
+    }
+    else{
+      guessCounter = 0;
+      alert("Incorrect! Try again.");
+    }
+    
   }
 }
 
@@ -101,7 +136,8 @@ const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 392,
-  4: 466.2
+  4: 466.2,
+  5: 530
 }
 function playTone(btn,len){ 
   o.frequency.value = freqMap[btn]
